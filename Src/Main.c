778035a3895b1fb47315237f093f58544f76fecc
MatthewDiamond <lightningdragon96@gmail.com>
2017-11-04 16:53:33 -0600

    Added United Mod

--- Src/Main.c
+++ Src/Main.c
@@ -4,6 +4,8 @@
 #define WINDOM_CIPHER_KEY 0x0B7E7759
 #define SEEDMOD_CIPHER_KEY 0x95127634
 #define MSVMOD_CIPHER_KEY 0x19870430
+#define SEEDMOD205_CIPHER_KEY 0xAC510B91
+#define UNITEDMOD_CIPHER_KEY 0x13322366
 
 #include <windows.h>
 #include <commdlg.h>
@@ -20,7 +22,7 @@ BROWSEINFO FolderDialog = { 0 };
 
 char Path[MAX_PATH];
 char Buffer[CHUNK_SIZE / sizeof(int)];
-int Keys[] = { WINDOM_CIPHER_KEY, SEEDMOD_CIPHER_KEY, MSVMOD_CIPHER_KEY };
+int Keys[] = { WINDOM_CIPHER_KEY, SEEDMOD_CIPHER_KEY, MSVMOD_CIPHER_KEY, SEEDMOD205_CIPHER_KEY, UNITEDMOD_CIPHER_KEY};
 char* ValidExtensions[] = { ".ani", ".fx", ".mpd", ".sdt", ".hod", ".ANI", ".FX", ".MPD", ".SDT", ".HOD" };
 
 // XORs a file an int at a time till no longer possible
@@ -37,6 +39,11 @@ void CipherFile(int key, char* path)
 		while (isAtEndOfFile)
 		{
 			chunkSize = fread(Buffer, sizeof(char), sizeof(Buffer), fileHandle);
+
+			if (chunkSize < 1)
+            {
+                break;
+            }
 
 			if (feof(fileHandle))
 			{
@@ -193,6 +200,8 @@ int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdL
 	SendDlgItemMessage(dialogHadle, IDC_DROP_DOWN, CB_ADDSTRING, 0, (LPARAM)"Windom Mode");
 	SendDlgItemMessage(dialogHadle, IDC_DROP_DOWN, CB_ADDSTRING, 0, (LPARAM)"SeedMod Mode");
 	SendDlgItemMessage(dialogHadle, IDC_DROP_DOWN, CB_ADDSTRING, 0, (LPARAM)"MSV_MOD Mode");
+	SendDlgItemMessage(dialogHadle, IDC_DROP_DOWN, CB_ADDSTRING, 0, (LPARAM)"SeedMod 2.0.5");
+	SendDlgItemMessage(dialogHadle, IDC_DROP_DOWN, CB_ADDSTRING, 0, (LPARAM)"United Mod");
 	SendDlgItemMessage(dialogHadle, IDC_DROP_DOWN, CB_SETCURSEL, 0, 0);
 	CheckDlgButton(dialogHadle, IDC_CHECK_BOX, BST_CHECKED);
 
